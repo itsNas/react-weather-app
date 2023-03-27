a0f91016dd1a59ef5f2196d98899b2c027c3f411
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-03-27 16:28:50 +0800

    add basic function for each components

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CurrentWeather() {
+  return <div className="CurrentWeather">Current weather</div>;
+}
+
+export default CurrentWeather;
---
+++ src/components/Forecast.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Forecast() {
+  return <div className="Forecast">Forecast</div>;
+}
+
+export default Forecast;
