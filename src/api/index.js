193283b9c4de2955cf3ab121b77365b609c1c975
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-03-28 20:37:18 +0800

    add method for searchBox

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+export const GeoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bac3cc626fmshedc24300a0f5e93p13ff4fjsn040a85627261",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, GeoApiOptions } from "../api";
 
 function SearchBox({ onSearchChange }) {
   const [search, setSearch] = useState("");
+
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
+  };
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        GeoApiOptions
+      );
+      const response_1 = await response.json();
+      return {
+        options: response_1.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          };
+        }),
+      };
+    } catch (err) {
+      return console.error(err);
+    }
   };
 
   return (
@@ -15,6 +37,7 @@ function SearchBox({ onSearchChange }) {
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
       />
     </div>
   );
