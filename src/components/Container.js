b7b6b0bf9eb9177efbb37a006747dd69ae505ef0
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-04-02 23:17:17 +0800

    fix the data fetched not able to be used

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,18 +1,11 @@
 import React from "react";
 import CurrentWeather from "./CurrentWeather";
 import Forecast from "./Forecast";
-import { useWeather } from "../hooks";
 
-function Container() {
-  const { weatherData } = useWeather();
-
+function Container({ data, loading, error }) {
   return (
     <div className="container">
-      {weatherData ? (
-        <CurrentWeather data={weatherData} />
-      ) : (
-        <div>Loading...</div>
-      )}
+      <CurrentWeather data={data} loading={loading} error={error} />
       <Forecast />
     </div>
   );
