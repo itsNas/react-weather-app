30047a9a70835ff6daeba17e1d12034ac1008df1
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-04-03 18:23:54 +0800

    rearrange the forecast component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CurrentWeather from "./CurrentWeather";
-import Forecast from "./Forecast";
+import Forecast from "./InfoSide";
 
 function Container({ data, loading, error }) {
   return (
