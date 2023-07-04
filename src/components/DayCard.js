1c3896e3bc13a78a27f5a8c6859c92247ce5d7c6
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-07-04 19:06:37 +0800

    daycard and infoside

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -4,35 +4,39 @@ import React, { useState } from "react";
 function DayCard({ data, onClickIndex }) {
   const [selectedCard, setSelectedCard] = useState(0);
 
-  const onClick = (index) => {
+  const handleClick = (index) => {
     setSelectedCard(index);
     // call the onClick prop with the selected index
     onClickIndex(index);
   };
 
   const days = data.forecast.forecastday.slice(0, 3);
-  const dayList = days.map((day, index) => (
-    <li
-      key={index}
-      className={selectedCard === index ? "active" : ""}
-      onClick={() => onClick(index)}
-    >
-      <img
-        className="day-icon"
-        src={day.day.condition.icon}
-        alt="weather icon"
-      ></img>
-      <span className="day-name">{moment(day.date).format("dddd")}</span>
-      <span className="day-temp">{day.day.avgtemp_c}°C</span>
-    </li>
-  ));
 
   return (
     <div>
       <div className="week-container">
-        <ul className="week-list">
-          {dayList}
-          <div className="clear"></div>
+        <ul className="week-list list-none p-0 mx-5 my-8 rounded-lg flex justify-center items-center bg-slate-400/50 drop-shadow-xl">
+          {days.map((day, index) => (
+            <li
+              key={index}
+              className={
+                selectedCard === index
+                  ? " bg-slate-50 text-black"
+                  : " float-left p-5 cursor-pointer"
+              }
+              onClick={() => handleClick(index)}
+            >
+              <img
+                className="day-icon"
+                src={day.day.condition.icon}
+                alt="weather icon"
+              ></img>
+              <span className="day-name block text-center">
+                {moment(day.date).format("dddd")}
+              </span>
+              <span className="day-temp">{day.day.avgtemp_c}°C</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/components/InfoSide.js
+++ src/components/InfoSide.js
@@ -3,49 +3,49 @@ import DayCard from "./DayCard";
 import { WiMoonrise, WiMoonset, WiSunrise, WiSunset } from "react-icons/wi";
 
 function InfoSide({ data }) {
-  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const day = data.forecast.forecastday[selectedIndex].day;
 
   return (
     <div className="info-container">
-      <div className="weather-info">
+      <div className="weather-info flex justify-evenly">
         <div className="astro-info">
-          <div className="sun-card">
-            <div className="sunrise">
-              <WiSunrise />
+          <div className="sun card">
+            <div className="sunrise cardInfo">
+              <WiSunrise className="icon" />
               <p>Sunrise</p>
               <p>3.30</p>
             </div>
-            <div className="sunset">
-              <WiSunset />
+            <div className="sunset cardInfo">
+              <WiSunset className="icon" />
               <p>Sunset</p>
               <p>3.30</p>
             </div>
           </div>
-          <div className="moon-card">
-            <div className="moonrise">
-              <WiMoonrise />
+          <div className="moon card">
+            <div className="moonrise cardInfo">
+              <WiMoonrise className="icon" />
               <p>Moonrise</p>
               <p>4</p>
             </div>
-            <div className="moonset">
-              <WiMoonset />
+            <div className="moonset cardInfo">
+              <WiMoonset className="icon" />
               <p>Moonset</p>
               <p>5</p>
             </div>
           </div>
         </div>
-        <div className="today-info ">
-          <div className="info precipitation">
+        <div className="today-info card ">
+          <div className="info precipitation cardInfo">
             <p className="title">PRECIPITATION</p>
             <p className="value">{day.daily_chance_of_rain} %</p>
           </div>
-          <div className="info humidity">
+          <div className="info humidity cardInfo">
             <p className="title">HUMIDITY</p>
             <p className="value">{day.avghumidity} %</p>
           </div>
-          <div className="info wind">
+          <div className="info wind cardInfo">
             <p className="title">WIND</p>
             <p className="value">{day.maxwind_kph} km/h</p>
           </div>
